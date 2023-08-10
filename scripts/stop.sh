4960e38d93d3a33e53e224fb8400ffb8784e710d
Andy Schunke <schunke.andy@gmail.com>
2023-08-10 20:58:12 +0200

    debug

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,5 +2,9 @@
 
 # stop docker compose 
 docker-compose down
-# delete all containers
-docker rm $(docker ps -a -q)
+# before removing all containers, check if there are any containers running, if so, stop them and remove them
+if [ "$(docker ps -q)" ]; then
+    docker rm $(docker ps -a -q)
+fi
+
+
