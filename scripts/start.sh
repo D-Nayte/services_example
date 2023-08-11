173088d9153ebfd792113f83cfb95d17cffeb4e7
Andy Schunke <schunke.andy@gmail.com>
2023-08-11 16:25:21 +0200

    ds

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,5 +3,10 @@
 # start docker compose from home/ec2-user
 cd ..
 
+# echo the current directory
+echo "Current directory: $(pwd)"
+# echo with ls
+echo "Current directory: $(ls)"
+
 # start docker compose
 docker-compose up -d --build
